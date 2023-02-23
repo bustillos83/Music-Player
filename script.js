8ad801a57e6156c3146ee01f8044c6f1bd61e29e
Fernando Bustillos <bustillos007@gmail.com>
2023-02-22 23:45:47 -0500

    added event listeners

--- script.js
+++ script.js
@@ -1,8 +1,35 @@
+const image = document.querySelector("img");
+const title = document.getElementById("title");
+const artist = document.getElementById("artist");
 const music = document.querySelector("audio");
 const prevBtn = document.getElementById("prev");
 const playBtn = document.getElementById("play");
 const nextBtn = document.getElementById("next");
 
+// Music
+
+const songs = [
+  {
+    name: "Ruff Ryders Anthem",
+    displayName: "Ruff Ryders Anthem",
+    artist: "DMX",
+  },
+  {
+    name: "Boogie Wonderland",
+    displayName: "Boogie Wonderland",
+    artist: "Earth Wind & Fire",
+  },
+  {
+    name: "Peter Schilling- Major Tom",
+    displayName: "Major Tom",
+    artist: "Peter Schilling",
+  },
+  {
+    name: "Take on Me",
+    displayName: "Take on Me",
+    artist: "A-Ha",
+  },
+];
 //Check if playing
 let isPlaying = false;
 
@@ -24,3 +51,37 @@ function pauseSong() {
 
 // Play or Pause Event Listener
 playBtn.addEventListener("click", () => (isPlaying ? pauseSong() : playSong()));
+
+// Update DOM
+function loadSong(song) {
+  title.textContent = song.displayName;
+  artist.textContent = song.artist;
+  music.src = `music/${song.name}.mp3`;
+  image.src = `img/${song.name}.jpg`;
+}
+
+// Current Song
+let songIndex = 0;
+
+// Previous Song
+function prevSong() {
+  songIndex--;
+  console.log(songIndex);
+  loadSong(songs[songIndex]);
+  playSong();
+}
+
+// Next Song
+function nextSong() {
+  songIndex++;
+  console.log(songIndex);
+  loadSong(songs[songIndex]);
+  playSong();
+}
+
+// On Load - Select First Song
+loadSong(songs[songIndex]);
+
+// Event Listener
+prevBtn.addEventListener("click", prevSong);
+nextBtn.addEventListener("click", nextSong);
